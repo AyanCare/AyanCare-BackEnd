e9c69700af9fa3a76a6fd04665585b6e09904c39
GUSTAVO SOUZA TENORIO DE BARROS <22282225@dsn127m1246730.cfp127aluno.local>
2023-09-27 14:14:10 -0300

    Model: Finalizado DAO contato

--- model/DAO/contatoDAO.js
+++ model/DAO/contatoDAO.js
@@ -1,5 +1,5 @@
 /**************************************************************************************
- * Objetivo: Responsável pela manipulação de dados dos Responsaveis no Banco de Dados.
+ * Objetivo: Responsável pela manipulação de dados dos contato no Banco de Dados.
  * Data: 26/09/2023
  * Autor: Gustavo Souza
  * Versão: 1.0
@@ -14,19 +14,19 @@ var prisma = new PrismaClient()
 
 /********************Selects************************** */
 
-const selectAllResponsaveis = async function() {
+const selectAllContato = async function() {
     
 
     //ScriptSQL para buscar todos os itens do BD
-    let sql = 'SELECT * FROM tbl_contado'
+    let sql = 'SELECT * FROM tbl_contato'
 
     //$queryRawUnsafe(sql) - Permite interpretar uma variável como sendo um scriptSQL
     //$queryRaw('SELECT * FROM tbl_aluno') - Executa diretamente o script dentro do método
-    let rscontado = await prisma.$queryRawUnsafe(sql)
+    let rscontato = await prisma.$queryRawUnsafe(sql)
 
     //Valida se o BD retornou algum registro
-    if (rscontado.length > 0) {
-        return rscontado
+    if (rscontato.length > 0) {
+        return rscontato
     } else {
         return false
     }
@@ -38,16 +38,90 @@ const selectAllResponsaveis = async function() {
 
 /******************Select pelo ID************************ */
 
-const selectcontadoById = async function (idContado) {
-    let sql = `SELECT * FROM tbl_contado where id = ${idContado}`
+const selectcontatoById = async function (idcontato) {
+    let sql = `SELECT * FROM tbl_contato where id = ${idcontato}`
 
-    let rscontado = await prisma.$queryRawUnsafe(sql)
+    let rscontato = await prisma.$queryRawUnsafe(sql)
 
-    if (rscontado.length > 0) {
-        return rscontado
+    if (rscontato.length > 0) {
+        return rscontato
     } else {
         return false
     }
 }
 
 
+
+/***********************Inserte***************************** */
+
+const insertContato = async function( dadosContato){
+
+    let sql = `insert into tbl_contato(
+        nome,
+        numero,
+        local,
+        id_paciente,
+        id_status_contato
+    ) values(
+        '${dadosContato.nome}',
+        '${dadosContato.numero}',
+        '${dadosContato.local}',
+        ${dadosContato.id_paciente},
+        ${dadosContato.id_status_contato},
+    )`
+
+    let resultStatus = await prisma.$executeRawUnsafe(sql)
+
+    if (resultStatus) {
+        return true
+    } else {
+        return false
+    }
+
+
+}
+
+
+
+/****************** Updates  *********************************** */
+
+const updateContato = async function(dadosContato){
+
+    let sql = `update tbl_contato set 
+            nome   = '${dadosContato.nome}',
+            numero = '${dadosContato.numero}',
+            local  = '${dadosContato.local}'
+        where id = ${dadosContato.id} `
+    let resultStatus = await prisma.$executeRawUnsafe(sql)
+    
+    if (resultStatus){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+
+
+/************************** Deletes ******************************/
+const deleteContato = async function (idContato) {
+    let sql = `delete from tbl_contato where id = ${idContato}`
+
+    let resultStatus = await prisma.$executeRawUnsafe(sql)
+
+    if (resultStatus) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+module.exports = {
+    deleteContato,
+    updateContato,
+    insertContato,
+    selectAllContato,
+    selectcontatoById
+}
