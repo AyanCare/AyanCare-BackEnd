a96748778346f59eb10abba13e7ecea4457c29f1
Lohannes <109982591+Lohannn@users.noreply.github.com>
2023-10-04 20:52:39 -0300

    Update controller_paciente.js

--- controller/controller_paciente.js
+++ controller/controller_paciente.js
@@ -64,7 +64,8 @@ const getPacienteByEmailAndSenhaAndNome = async function (dadosPaciente) {
 
             dadosPacienteJSON.token = tokenUser
             dadosPacienteJSON.status = messages.SUCCESS_REQUEST.status
-            dadosPacienteJSON.paciente = rsPaciente
+            dadosPacienteJSON.nome = rsPaciente.nome
+            dadosPacienteJSON.email = rsPaciente.email
             return dadosPacienteJSON
         } else {
             return messages.ERROR_NOT_FOUND
