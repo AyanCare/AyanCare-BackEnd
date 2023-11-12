05db75c256db4f0c2d4a2912f8999f4d531ebc8d
Lohannes <109982591+Lohannn@users.noreply.github.com>
2023-11-12 17:27:01 -0300

    Update medicamentoDAO.js

--- model/DAO/medicamentoDAO.js
+++ model/DAO/medicamentoDAO.js
@@ -31,10 +31,10 @@ const selectAllMedicamentos = async function () {
 
 }
 
-const selectMedicamentosNomes = async function () {
+const selectMedicamentosNomes = async function (idPaciente) {
 
     //scriptSQL para buscar todos os itens do BD
-    let sql = 'select DISTINCT tbl_medicamento.nome from tbl_medicamento;'
+    let sql = `select DISTINCT tbl_medicamento.nome from tbl_medicamento where id_paciente = ${idPaciente};`
 
     //$queryRawUnsafe(sql) - Permite interpretar uma variável como sendo um scriptSQL
     //$queryRaw('SELECT * FROM tbl_aluno') - Executa diretamente o script dentro do método
