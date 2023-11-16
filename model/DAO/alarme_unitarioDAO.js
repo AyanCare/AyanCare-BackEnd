2044daed57f4108218f676f500d06ba6be0d6ff6
Lohannn <lohannes.s26@gmail.com>
2023-11-16 14:06:36 -0300

    ajuste no index

--- model/DAO/alarme_unitarioDAO.js
+++ model/DAO/alarme_unitarioDAO.js
@@ -7,6 +7,7 @@
 
 //Import da biblioteca do prisma client
 var { PrismaClient } = require('@prisma/client');
+const moment = require("moment");
 
 //Instância da classe PrismaClient
 var prisma = new PrismaClient()
@@ -15,7 +16,8 @@ const selectAllAlarmes = async function (idPaciente) {
     let sql = `select tbl_alarme_unitario_status.id as id_alarme_unitario,
 					  tbl_medicamento.id as id_medicamento,
                       tbl_medicamento.nome as medicamento,
-                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i:%s') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_medida.id as id_medida, tbl_medida.tipo as medida, tbl_medida.sigla as medida_sigla,
                       tbl_status_alarme.nome as status,
                       tbl_paciente.id as id_paciente,
                       tbl_paciente.nome as paciente
@@ -27,7 +29,9 @@ const selectAllAlarmes = async function (idPaciente) {
         inner join tbl_medicamento
     on tbl_alarme_medicamento.id_medicamento = tbl_medicamento.id
 		inner join tbl_paciente
-	on tbl_paciente.id = tbl_medicamento.id_paciente`
+	on tbl_paciente.id = tbl_medicamento.id_paciente
+        inner join tbl_medida
+    on tbl_medida.id = tbl_medicamento.id_medida`
 
     let rsAlarme = await prisma.$queryRawUnsafe(sql)
 
@@ -43,7 +47,8 @@ const selectAlarmeByIdPaciente = async function (idPaciente) {
     let sql = `select tbl_alarme_unitario_status.id as id_alarme_unitario,
 					  tbl_medicamento.id as id_medicamento,
                       tbl_medicamento.nome as medicamento,
-                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i:%s') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_medida.id as id_medida, tbl_medida.tipo as medida, tbl_medida.sigla as medida_sigla,
                       tbl_status_alarme.nome as status,
                       tbl_paciente.id as id_paciente,
                       tbl_paciente.nome as paciente
@@ -56,6 +61,8 @@ const selectAlarmeByIdPaciente = async function (idPaciente) {
     on tbl_alarme_medicamento.id_medicamento = tbl_medicamento.id
 		inner join tbl_paciente
 	on tbl_paciente.id = tbl_medicamento.id_paciente
+        inner join tbl_medida
+    on tbl_medida.id = tbl_medicamento.id_medida
     where tbl_paciente.id = ${idPaciente};`
 
     let rsAlarme = await prisma.$queryRawUnsafe(sql)
@@ -73,7 +80,8 @@ const selectAlarmeById = async function (idAlarme) {
     let sql = `select tbl_alarme_unitario_status.id as id_alarme_unitario,
 					  tbl_medicamento.id as id_medicamento,
                       tbl_medicamento.nome as medicamento,
-                      tbl_alarme_medicamento.id as id_alarme, DATE_FORMAT(tbl_alarme_medicamento.dia, '%d/%m/%Y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, TIME_FORMAT(tbl_alarme_unitario_status.horario, '%H:%i:%s') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_alarme_medicamento.id as id_alarme, DATE_FORMAT(tbl_alarme_medicamento.dia, '%d/%m/%Y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, TIME_FORMAT(tbl_alarme_unitario_status.horario, '%H:%i') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_medida.id as id_medida, tbl_medida.tipo as medida, tbl_medida.sigla as medida_sigla,
                       tbl_status_alarme.nome as status,
                       tbl_paciente.id as id_paciente,
                       tbl_paciente.nome as paciente
@@ -86,6 +94,8 @@ const selectAlarmeById = async function (idAlarme) {
     on tbl_alarme_medicamento.id_medicamento = tbl_medicamento.id
 		inner join tbl_paciente
 	on tbl_paciente.id = tbl_medicamento.id_paciente
+        inner join tbl_medida
+    on tbl_medida.id = tbl_medicamento.id_medida
     where tbl_alarme_unitario_status.id = ${idAlarme}`
 
     let rsAlarme = await prisma.$queryRawUnsafe(sql)
@@ -101,7 +111,8 @@ const selectAlarmeByIdMedicamento = async function (idMedicamento) {
     let sql = `select tbl_alarme_unitario_status.id as id_alarme_unitario,
 					  tbl_medicamento.id as id_medicamento,
                       tbl_medicamento.nome as medicamento,
-                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i:%s') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_medida.id as id_medida, tbl_medida.tipo as medida, tbl_medida.sigla as medida_sigla,
                       tbl_status_alarme.nome as status,
                       tbl_paciente.id as id_paciente,
                       tbl_paciente.nome as paciente
@@ -114,6 +125,8 @@ const selectAlarmeByIdMedicamento = async function (idMedicamento) {
     on tbl_alarme_medicamento.id_medicamento = tbl_medicamento.id
 		inner join tbl_paciente
 	on tbl_paciente.id = tbl_medicamento.id_paciente
+        inner join tbl_medida
+    on tbl_medida.id = tbl_medicamento.id_medida
     where tbl_medicamento.id = ${idMedicamento}`
 
     let rsAlarme = await prisma.$queryRawUnsafe(sql)
@@ -129,7 +142,8 @@ const selectLastId = async function () {
     let sql = `select tbl_alarme_unitario_status.id as id_alarme_unitario,
 					  tbl_medicamento.id as id_medicamento,
                       tbl_medicamento.nome as medicamento,
-                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_medicamento.dia, '%d/%m/%y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%h:%i:%s') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_alarme_medicamento.id as id_alarme, date_format(tbl_alarme_unitario_status.dia, '%d/%m/%Y') as data_criacao, tbl_alarme_medicamento.intervalo as intervalo, time_format(tbl_alarme_unitario_status.horario, '%H:%i') as horario_inicial, tbl_alarme_unitario_status.quantidade as quantidade_retirada,
+                      tbl_medida.id as id_medida, tbl_medida.tipo as medida, tbl_medida.sigla as medida_sigla,
                       tbl_status_alarme.nome as status,
                       tbl_paciente.id as id_paciente,
                       tbl_paciente.nome as paciente
@@ -142,6 +156,8 @@ const selectLastId = async function () {
     on tbl_alarme_medicamento.id_medicamento = tbl_medicamento.id
 		inner join tbl_paciente
 	on tbl_paciente.id = tbl_medicamento.id_paciente
+        inner join tbl_medida
+    on tbl_medida.id = tbl_medicamento.id_medida
     order by tbl_alarme_unitario_status.id desc limit 1;`
 
     let rsAlarme = await prisma.$queryRawUnsafe(sql)
@@ -156,6 +172,7 @@ const selectLastId = async function () {
 /***********************Inserte***************************** */
 
 const insertAlarme = async function (dadosAlarme) {
+
     let sql = `insert into tbl_alarme_unitario_status(
         dia,
         horario,
@@ -163,8 +180,8 @@ const insertAlarme = async function (dadosAlarme) {
         quantidade,
         id_alarme_medicamento
     ) values(
-        curdate(),
-        curtime(),
+        CURRENT_DATE,
+        CURRENT_TIME,
         3,
         '${dadosAlarme.quantidade}',
         ${dadosAlarme.id_alarme_medicamento}
