5e7e7261747cd38836a397df7a0f418eadebea01
Lohannes <109982591+Lohannn@users.noreply.github.com>
2023-11-29 16:52:27 -0300

    Update notificacaoDAO.js

--- model/DAO/notificacaoDAO.js
+++ model/DAO/notificacaoDAO.js
@@ -45,6 +45,36 @@ const selectAllNotificacoes = async function () {
 
 }
 
+const selectNotificacoesByCuidadorAndPaciente = async function (idPaciente, idCuidador) {
+
+    //scriptSQL para buscar todos os itens do BD
+    let sql = `SELECT tbl_notificacao.nome as nome, tbl_notificacao.descricao, date_format(tbl_notificacao.data_criacao, '%d/%m/%Y') as data_criacao, time_format(tbl_notificacao.hora_criacao, '%H:%i') as hora_criacao,
+                      tbl_cuidador.id as id_cuidador, tbl_cuidador.nome as cuidador,
+		      tbl_paciente.id as id_paciente, tbl_paciente.nome as paciente
+    FROM tbl_notificacao
+        left join tbl_paciente_notificacao
+    on tbl_paciente_notificacao.id_notificacao = tbl_notificacao.id
+        left join tbl_cuidador_notificacao
+    on tbl_cuidador_notificacao.id_notificacao = tbl_notificacao.id
+        left join tbl_paciente
+    on tbl_paciente_notificacao.id_paciente = tbl_paciente.id
+        left join tbl_cuidador
+    on tbl_cuidador_notificacao.id_cuidador = tbl_cuidador.id
+    where tbl_paciente.id = ${idPaciente} and tbl_cuidador.id = ${idCuidador}`
+
+    //$queryRawUnsafe(sql) - Permite interpretar uma variável como sendo um scriptSQL
+    //$queryRaw(`SELECT * FROM tbl_aluno`) - Executa diretamente o script dentro do método
+    let rsNotificacao = await prisma.$queryRawUnsafe(sql)
+
+    //Valida se o BD retornou algum registro
+    if (rsNotificacao.length > 0) {
+        return rsNotificacao
+    } else {
+        return false
+    }
+
+}
+
 const selectAllNotificacoesByPaciente = async function (idPaciente) {
 
     //scriptSQL para buscar todos os itens do BD
@@ -232,5 +262,6 @@ module.exports = {
     selectNotificacaoById,
     selectLastId,
     insertNotificacao,
-    selectAllModificacoesDePaciente
+    selectAllModificacoesDePaciente,
+    selectNotificacoesByCuidadorAndPaciente
 }
